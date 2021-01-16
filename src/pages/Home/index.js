6c2f05531db974ee87fa943bcedc49569070e692
carloshrf <carlos_6282010@hotmail.com>
2021-01-16 12:36:11 -0300

    iniciando criação do componente 'curso'

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Container } from './styles';
 
+import CourseItem from '../../components/CourseItem';
+
 const Home = () => {
   return (
     <Container>
       <h1>Home</h1>
+      <CourseItem 
+        name="Cursos de javascript"
+        platform="Rocketseat"
+      />
+      <CourseItem 
+        name="Banco de dados "
+        platform="Rocketseat"
+      />
     </Container>
   )
 }
