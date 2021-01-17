2548cc321f2ea59703f7e99bb056cc601fc4085b
João Victor <dante@arch.localdomain>
2021-01-17 01:01:32 -0300

    Pequenas mudanças

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import GlobalStyles from './styles/Global'
+import User from './pages/User';
+import GlobalStyles from './styles/Global';
 
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <User />
       <GlobalStyles />
     </>
   );
